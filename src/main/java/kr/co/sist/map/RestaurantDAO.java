ed37ac4d54e59fb45fd0765f869bdfc2f68cd3b5
rurouni5181 <rurouni5181@daum.net>
2025-12-18 11:40:35 +0900

    JSP마지막

--- src/main/java/kr/co/sist/map/RestaurantDAO.java
+++ src/main/java/kr/co/sist/map/RestaurantDAO.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import kr.co.sist.board.BoardDTO;
 import kr.co.sist.dao.DbConn;
 
 public class RestaurantDAO {
@@ -56,6 +55,11 @@ public class RestaurantDAO {
 			while(rs.next()) {
 				rDTO=new RestaurantDTO();
 				rDTO.setRest_num(rs.getInt("rest_num"));
+				rDTO.setRest_name(rs.getString("rest_name"));
+				rDTO.setMenu(rs.getString("menu"));
+				rDTO.setLat(rs.getDouble("lat"));
+				rDTO.setLng(rs.getDouble("lng"));
+				rDTO.setInput_date(rs.getDate("input_date"));
 				
 				list.add(rDTO);
 			}//end while
@@ -70,7 +74,48 @@ public class RestaurantDAO {
 	}//selectAllRestaurant
 	
 	public void insertRestaurant( RestaurantDTO rDTO)throws SQLException{
+	
+DbConn dbCon=DbConn.getInstance("jdbc/dbcp");
+		
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		
+		try {
+		//1.JNDI사용객체 생성
+		//2.DataSource 얻기
+		//3.DataSource에서 Connection 얻기
+			con=dbCon.getConn();
+		//4.쿼리문 생성객체 얻기
+			String insertRestaurant
+="insert into restaurant(rest_num,id,rest_name,menu,info,lat,lng)values( seq_rest.nextval,?,?,?,?,?,?)";
+			pstmt=con.prepareStatement(insertRestaurant);
+			
+		//5.바인드변수 값 설정
+			pstmt.setString(1, rDTO.getId());
+			pstmt.setString(2, rDTO.getRest_name());
+			pstmt.setString(3, rDTO.getMenu());
+			pstmt.setString(4, rDTO.getInfo());
+			pstmt.setDouble(5, rDTO.getLat());
+			pstmt.setDouble(6, rDTO.getLng());
+		//6.쿼리문 수행 후 결과 얻기
+			pstmt.executeUpdate();
+		}finally {
+		//7.연결끊기
+			dbCon.dbClose(null, pstmt, con);
+		}//end finally		
 		
 	}//insertRestaurant
 	
 }//class
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/kr/co/sist/map/RestaurantService.java
@@ -0,0 +1,49 @@
+package kr.co.sist.map;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+
+public class RestaurantService {
+
+	private static RestaurantService rs;
+	private RestaurantService() {
+	}
+	
+	public static RestaurantService getInstance() {
+		if(rs == null) {
+			rs=new RestaurantService();
+		}//end if
+		return rs;
+	}//getInstance
+
+	public List<RestaurantDTO> searchRestaurant(String id){
+		List<RestaurantDTO> list=null;
+		
+		RestaurantDAO rDAO=RestaurantDAO.getInstance();
+		try {
+			list=rDAO.selectAllRestaurant(id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}//end catch
+		
+		return list;
+	}//searchRestaurant
+	
+	public String addRestaurant( RestaurantDTO rDTO) {
+		JSONObject jsonObj=new JSONObject();
+		jsonObj.put("resultFlag", false);
+		
+		RestaurantDAO rDAO=RestaurantDAO.getInstance();
+		try {
+			rDAO.insertRestaurant(rDTO);
+			jsonObj.put("resultFlag", true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}//end catch
+		
+		return jsonObj.toJSONString();
+	}//addRestaurant
+	
+}
